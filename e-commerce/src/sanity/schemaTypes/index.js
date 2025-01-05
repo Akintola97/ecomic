6a5a42df80d39192127693e4000b71842a64ddd3
Akintola97 <84879342+Akintola97@users.noreply.github.com>
2025-01-04 18:22:35 -0600

    Sanity io store products

--- e-commerce/src/sanity/schemaTypes/index.js
+++ e-commerce/src/sanity/schemaTypes/index.js
@@ -1,6 +1,7 @@
 import { bannerContent } from "../schemas/bannerContent";
 import { featuredProducts } from "../schemas/featuredProducts";
-import { storeProducts } from "../schemas/StoreProducts";
+import { storeProducts } from "../schemas/storeProducts";
+
 
 export const schema = {
   types: [bannerContent, featuredProducts, storeProducts],
