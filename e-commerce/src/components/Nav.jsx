f5534cdd682bf22118cf22ac230ada62dc1814f7
Akintola97 <84879342+Akintola97@users.noreply.github.com>
2025-01-08 19:14:57 -0600

    Title

--- e-commerce/src/components/Nav.jsx
+++ e-commerce/src/components/Nav.jsx
@@ -53,7 +53,7 @@ export default async function Nav() {
     <nav className="w-full h-[8vh] fixed top-0 left-0 right-0 bg-black flex items-center justify-between text-white px-2 z-50 font-poppins capitalize">
       <div className="flex items-center text-white p-3">
         <Link href="/">
-          <h1 className="font-bold">Blog</h1>
+          <h1 className="font-bold">Comics</h1>
         </Link>
       </div>
       <div className="flex items-center space-x-4">
