6455f696326fe6802bf8358a683cff432b2b802b
Akintola97 <84879342+Akintola97@users.noreply.github.com>
2025-04-08 00:16:13 -0500

    update

--- e-commerce/src/app/cart/page.js
+++ e-commerce/src/app/cart/page.js
@@ -1,9 +1,18 @@
 import CartData from "@/components/CartData";
+import { redirect } from "next/navigation";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
 export default async function Cart() {
+  const {isAuthenticated} = getKindeServerSession();
+  const auth = await isAuthenticated();
+
+
+  if(!auth){
+    redirect('/')
+  }
   return (
     <>
-      <CartData />
+      <CartData isUserAuthenticated = {auth} />
     </>
   );
-}
+};
